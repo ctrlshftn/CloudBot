6b97e85fe0206d3cbfcdeb9d68f152b3e19e8702
Andrew Edwards <andrewthomasedwards@gmail.com>
2017-02-20 05:48:58 -0500

    disable in lmr

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -6,10 +6,15 @@ from cloudbot import hook
 # Then run python3 setup.py install
 # At the time of this commit version 1.0.2 works but is not yet in pypi
 
+# Define channels that have opted out of this command.
+
+opt_out = ['#linuxmasterrace']
  
-cb = Cleverbot()
+cb = Cleverbot('gonzobot')
 
 @hook.command("ask", "gonzo", "gonzobot", "cleverbot", "cb")
-def chitchat(text):
+def chitchat(text, chan):
     """chat with cleverbot.com"""
+    if chan in opt_out:
+        return
     return cb.ask(text)
